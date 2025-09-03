e7f2bdee122a6dadeb0523ab67a22e5fdf88bc4b
Lautaro Alloco <lautaroalloco@gmail.com>
2025-09-03 10:27:03 -0300

    7

---
+++ ej7/7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+    float a = 5;
+    float b = 3;
+    float c = 2;
+    float d = 1;
+
+    printf("%f",(a + b * c / d));
+
+    return 0;
+}
+
+/*a = 5, b = 3, c = 2, d = 1
+a + b * c / d
+a % b
+a == b, a != b
+a & b, a | b
+∼a
+a && b, a || b
+a << 1
+a >> 1
+a += b, a -= b, a *= b, a /= b, a %= b*/
